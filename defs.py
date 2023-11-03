efe344165c9546f6599342820f77f3a5b6dd8c17
daishuge <play13661948263@gmail.com>
2023-11-03 17:54:17 +0800

    Fix pygame module not found error and improve code

--- defs.py
+++ defs.py
@@ -3,11 +3,9 @@ import re
 import openai
 import time
 
-def terminal(command):    # terminal("ipconfig")
-    #传入终端命令,返回终端输出
+def terminal(command):
     try:
-        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
-
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True, encoding='utf-8', errors='ignore')
         stdout = result.stdout
         stderr = result.stderr
         
@@ -20,23 +18,51 @@ def terminal(command):    # terminal("ipconfig")
     except Exception as e:
         return str(e)
 
-def get_code(md_str):   # code_list=get_code(md_str)
+
+# def get_code(md_str):   # code_list=get_code(md_str)
+#     '''
+#     传入md文本,返回代码块列表
+#     调用方法:
+#     code_list=get_code(md_str)
+#     for item in code_list:
+#         print(item)
+#     '''
+#     code_blocks = re.findall(r"```(?:[a-zA-Z]+\n)?([\s\S]*?)```", md_str)
+    
+#     code_blocks = [block.strip() for block in code_blocks]
+    
+#     return code_blocks
+
+import re
+
+def get_code(md_str):
     '''
-    传入md文本,返回代码块列表
+    传入md文本,返回两种代码块列表：Python代码块和终端命令块
     调用方法:
-    code_list=get_code(md_str)
-    for item in code_list:
-        print(item)
+    python_code_list, terminal_code_list = get_code(md_str)
+    for code in python_code_list:
+        print("Python Code:", code)
+    for code in terminal_code_list:
+        print("Terminal Command:", code)
     '''
-    code_blocks = re.findall(r"```(?:[a-zA-Z]+\n)?([\s\S]*?)```", md_str)
+
+    # 匹配Python代码块
+    python_code_blocks = re.findall(r"```python\n(.*?)```", md_str, re.DOTALL)
+
+    # 匹配终端命令块
+    terminal_code_blocks = re.findall(r"```(bash|shell)\n(.*?)```", md_str, re.DOTALL)
+    terminal_code_blocks = [block[1] for block in terminal_code_blocks]  # 从元组中取出命令部分
+
+    # 移除多余的空格
+    python_code_blocks = [block.strip() for block in python_code_blocks]
+    terminal_code_blocks = [block.strip() for block in terminal_code_blocks]
     
-    code_blocks = [block.strip() for block in code_blocks]
-    
-    return code_blocks
+    return python_code_blocks, terminal_code_blocks
+
   
 def fake_api(query,mod,if_print):     # fake_api("hi! are you gpt?",True,1,"gpt-3.5-turbo")
 
-    openai.api_key = "YOUR_API_KEY"
+    openai.api_key = "fk-yU3UOSY13E9WsAkKBTrkq1KiiqIsAXBs_J6OTh_yvJM"
     openai.api_base = "https://ai.fakeopen.com/v1/" 
 
     response = openai.ChatCompletion.create(
--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import config
 
 if_error = False
 
-error=[]
+error=''    # 用于存储错误信息
 
-def first_code():
+def first_code():   # 首次编辑并运行代码
+    global error
+
     user_input = input("You: ")
 
     code_in_md = defs.fake_api(config.ask_code + user_input, "gpt-3.5-turbo", True)
-    code_list = defs.get_code(code_in_md)
-    code = code_list[0]
+    python_code_list, shell_code_list = defs.get_code(code_in_md)
+    code = python_code_list[0]
 
     with open("run.py", "w", encoding="utf-8") as r:
         r.write(code)
@@ -23,19 +25,21 @@ def first_code():
 
     if "error" in gpt_check or "错" in gpt_check:
         defs.print_color("\n\n出错了!\n\n" + terminal_return, "red")
-        error.append(terminal_return+"\n"+code)
+
+        error = terminal_return+"\n"+code
         return 1
     
     else:
         defs.print_color("\n\n成功运行!\n\n" + terminal_return, "green")
         return 0
 
-def debug_code():
+def debug_code():   # debug, 或者安装库
+    global error
     ask = config.debug + error
-    fake_api = defs.fake_api(ask, "gpt-3.5-turbo", True)
+    reply = defs.fake_api(ask, "gpt-3.5-turbo", True)
 
-    code_list = defs.get_code(fake_api)
-    code = code_list[0]
+    python_code_list, shell_code_list = defs.get_code(reply)
+    code = python_code_list[0]
 
     with open("run.py", "w", encoding="utf-8") as r:
         r.write(code)
@@ -44,7 +48,7 @@ def debug_code():
 
     gpt_check = defs.fake_api(config.check_return + terminal_return+"\ncode:\n" + code, "gpt-3.5-turbo", True)
 
-    if "error" in gpt_check or "错" in gpt_check:
+    if "错" in gpt_check:
         defs.print_color("\n\n出错了!\n\n" + terminal_return, "red")
         error = terminal_return+"\n"+code
         return 1
